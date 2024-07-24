ce813ea063d2637b6a8c590c7ddfaabb25193f64
Kevin Fernandes Sanchez <kdfernan@espol.edu.ec>
2024-07-24 00:50:35 -0500

    update server.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8000,
+    port: 7000,
     host: true  // Para que se exponga la red
   }
 })
