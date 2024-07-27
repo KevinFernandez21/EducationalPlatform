4aa5dbfe88515627042c1924376122861c0584a8
Kevin Fernandes Sanchez <kdfernan@espol.edu.ec>
2024-07-26 23:21:28 -0500

    update home

--- src/shared/header.tsx
+++ src/shared/header.tsx
@@ -14,7 +14,7 @@ function Header() {
     return (
     <div className="p-4 flex justify-between overflow-x-hidden z-10">
         <div>
-            <a href="" className="flex items-center gap-1">
+            <a href="/" className="flex items-center gap-1">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V13.5Zm0 2.25h.008v.008H8.25v-.008Zm0 2.25h.008v.008H8.25V18Zm2.498-6.75h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V13.5Zm0 2.25h.007v.008h-.007v-.008Zm0 2.25h.007v.008h-.007V18Zm2.504-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5Zm0 2.25h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V18Zm2.498-6.75h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V13.5ZM8.25 6h7.5v2.25h-7.5V6ZM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 0 0 2.25 2.25h10.5a2.25 2.25 0 0 0 2.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0 0 12 2.25Z" />
                 </svg>
@@ -39,8 +39,8 @@ function Header() {
             </svg>
             <div className={`absolute top-14 right-0 m-2  px-4 bg-white  shadow-md shadow-gray-500 border-gray-200 rounded-xl transition-all duration-300 ease-in-out ${show ?"":"top-[-400px]"}`}>
                 <ul className="py-3">
-                    <li className="xl:hidden flex text-[20px]"><Link to={'/Books'}>Home</Link></li>
-                    <li className="xl:hidden flex text-[20px] mt-2"><Link to={'/Books'}>Aprende</Link></li>
+                    <li className="xl:hidden flex text-[20px]"><Link to={'/'}>Inicio</Link></li>
+                    <li className="xl:hidden flex text-[20px] mt-2"><Link to={'/Aprende'}>Aprende</Link></li>
                     <li className="text-[20px] mt-2"><Link to={'/Books'}>Libros</Link></li>
                     <li className="text-[20px] mt-2"><Link to={'/Practica'}>Practica</Link></li>
                     <li className="text-[20px] mt-2"><Link to={'/nosotros'}>¿Quienes Somos?</Link></li>
